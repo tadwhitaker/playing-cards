a53828c9a6dec9ec4311062d9cb5fcd85b159ef1
Tad <tad@whitakerresearch.com>
2014-08-06 13:45:28 -0700

    setting up test_settings.py

---
+++ war/war/test_settings.py
@@ -0,0 +1,13 @@
+__author__ = 'tadwhitaker'
+import os
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "test",
+        "USER": os.environ.get('PG_USER'),
+        "PASSWORD": os.environ.get('PG_PASSWORD'),
+        "HOST": "127.0.0.1",
+        "PORT": "",
+    }
+}
